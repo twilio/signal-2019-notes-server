fd4e2a2041c6409c51696b3adbf4ad39c20e441e
bgetson <bgetson@twilio.com>
2019-07-30 00:46:37 -0700

    v2.0 - protected with auth

--- routes/notes.js
+++ routes/notes.js
@@ -2,26 +2,40 @@ var express = require('express');
 var db = require('../db.js');
 var router = express.Router();
 
+const TokenValidator = require('twilio-flex-token-validator').validator;
+
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "X-FlexToken, Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
 
 router.get('/', function(req, res, next) {
-    db.getNote()
-        .then(notes => res.json({notes: notes}))
-        .catch(function(err) {
-            res.status(400).json(err.message);
+    TokenValidator(req.header('X-FlexToken'), process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
+        .then(tokenResult => {
+            db.getNote()
+                .then(notes => res.json({notes: notes}))
+                .catch(function(err) {
+                    res.status(400).json(err.message);
+                })
+        })
+        .catch(err => {
+            res.status(401).json(err);
         })
 });
 
 router.post('/', function(req, res, next) {
-    var notes = req.body.notes;
-    db.updateNote(notes)
-        .then(() => res.sendStatus(200))
-        .catch(function(err) {
-            res.status(400).json(err.message);
+    TokenValidator(req.header('X-FlexToken'), process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
+        .then(tokenResult => {
+            var notes = req.body.notes;
+            db.updateNote(notes)
+                .then(() => res.sendStatus(200))
+                .catch(function(err) {
+                    res.status(400).json(err.message);
+                })
+        })
+        .catch(err => {
+            res.status(401).json(err);
         })
 });
 
